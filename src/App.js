84ff65938b3b261937923371531145c965351971
Kelden Dorji <keldenddorji1997@gmail.com>
2019-10-08 03:13:10 +0530

    Made changes #3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './forms';
+import Weather from './Weather';
+
+const Api_Key = '6c141c2f3eabea919c566b0f5ba8480d';
+
+const getWeather = async (e) => {
+    e.preventDefault();
+    
+    const city = e.target.elements.city.value;
+
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    
+    const response = await api_call.json();
+    
+    console.log(response);
+    
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+        <Form loadWeather= {getWeather}/>
+        <Weather  />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
